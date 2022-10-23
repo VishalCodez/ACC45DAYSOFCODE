df5be0708543595470e72975d28b285ead600013
Vishal Sharma <78276141+VishalCodez@users.noreply.github.com>
2022-10-23 19:41:36 +0530

    🚨

---
+++ CODE/2-C++/Day-3.cpp
@@ -0,0 +1,24 @@
+// C++ program to check whether the character is an alphabet or not
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char ch;
+cout << “Enter a character: “;
+cin >> ch;
+cout << endl;
+if( (ch >= ‘a’ && ch <= ‘z’) || ( ch >= ‘A’ && ch <=’Z’))
+cout << ch <<” is an alphabet ” << endl;
+else
+cout << ch <<” is not an alphabet ” << endl;
+return 0;
+}
+
+//OUTPUT 
+
+// Input-
+// Enter a character:s
+// Output-
+// s is an alphabet
