92f7efab2729c3e4f8adbd6febb099d60ef5cec9
Vishal Sharma <78276141+VishalCodez@users.noreply.github.com>
2022-10-25 07:17:43 +0530

    🚨

---
+++ CODE/1-C/Day-5.c
@@ -0,0 +1,19 @@
+// C Program to find whether an integer is positive or negative
+#include<stdio.h>
+int main()
+{
+//Fill the code
+	int num;
+	scanf(“%d”,&num);
+	if(num > 0)
+		printf(“Positive”);
+	else
+		printf(“Negative”);
+	return 0;
+}
+
+//OUTPUT
+
+// 44
+
+// Positive
---
+++ CODE/2-C++/Why-5.cpp
@@ -0,0 +1,21 @@
+// C++ program to find whether an integer is positive or negative
+#include<iostream.h>
+using namespace std;
+
+int main()
+{
+//Fill the code
+int num;
+cin >> num;
+if(num > 0)
+cout << “Positive”;
+else
+cout << “Negative”;
+return 0;
+}
+
+//OUTPUT
+
+// 44
+
+// Positive
